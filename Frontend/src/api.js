38e8de97f1feb7df2eb4363f4eace12b99b9af7f
Shreyas Magdum <shreyasmagdum13@gmail.com>
2025-08-11 20:06:50 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // Your backend URL
+  baseURL: "http://localhost:5000", // Use your backend URL
 });
 
 export default API;
+
